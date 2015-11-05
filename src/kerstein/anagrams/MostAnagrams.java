5871bf043168bc65bed3913bbcf6fdfd2a1d65cb
renakerstein <renakerstein>
2015-11-05 16:55:51 -0500

    Modified anagrams algorithm

--- src/kerstein/anagrams/MostAnagrams.java
+++ src/kerstein/anagrams/MostAnagrams.java
@@ -6,9 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Map;
 
 public class MostAnagrams {
 
@@ -33,53 +31,50 @@ public class MostAnagrams {
 		} catch (FileNotFoundException ex) {
 
 		}
-		// sort each word in the dictionary
-		ArrayList<String> sortedDictionary = new ArrayList<String>();
-		Sort s = new Sort();
-		for (String w : dictionary) {
-			String newWord = s.sortDictionary(w);
-			sortedDictionary.add(newWord);
+		// create hash map
+		HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+
+		// add each sorted word as key, and get the arraylist at that point in
+		// the hash -
+		// to add the word with the same anagram into that arraylist
+		for (String s : dictionary) {
+			String word = s;
+			char[] ch = word.toCharArray();
+			Arrays.sort(ch);
+			String sortedWord = new String(ch);
+
+			ArrayList<String> anagramList = map.get(sortedWord);
+
+			// if it hasn't been initialized yet:
+			if (anagramList == null) {
+				anagramList = new ArrayList<String>();
+			}
+
+			anagramList.add(word);
+			map.put(sortedWord, anagramList);
+
 		}
 
-		Collections.sort(sortedDictionary);
+		// find anagram list with biggest size, and print it out
+		int biggestACount = 0;
+		ArrayList<String> biggestAnagramList = null;
+		for (String s : map.keySet()) {
 
-		// add the sorted words to a hash map
-		HashMap<String, Integer> map = new HashMap<String, Integer>();
+			// check for the largest size
+			if (map.get(s).size() > biggestACount) {
+				biggestACount = map.get(s).size();
+				biggestAnagramList = new ArrayList<String>();
+				biggestAnagramList.add(s);
+			}
 
-		for (String word : sortedDictionary) {
-			Integer value = map.get(word);
-			if (value == null) {
-				map.put(word, 1);
-
-			} else {
-				map.put(word, value + 1);
-
+			else if (map.get(s).size() == biggestACount) {
+				biggestAnagramList.add(s);
 			}
 		}
 
-		// find the group with the largest value
-		int largestValue = 0;
-		for (Map.Entry<String, Integer> entry : map.entrySet()) {
-
-			if (entry.getValue() > largestValue) {
-				largestValue = entry.getValue();
-			}
-
+		for (String word : biggestAnagramList) {
+			System.out.println(map.get(word));
 		}
 
-		// access the words that are part of the largest anagram group
-		for (int i = 0; i < dictionary.size(); i++) {
-			char[] chars = dictionary.get(i).toCharArray();
-			Arrays.sort(chars);
-			String sortedWord = new String(chars);
-			Integer value = map.get(sortedWord);
-
-			if (value == largestValue) {
-				System.out.println(dictionary.get(i));
-
-			}
-
-		}
 	}
-
 }
